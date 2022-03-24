cd5094bc45f7f0484e740118868b42a2cec08bda
josius <josius.amaro@gmail.com>
2022-03-24 16:38:29 -0300

    aula02 de 04-variaveis e tipos completa, faltam exercicios

--- 04-VariaveisETipos/script.js
+++ 04-VariaveisETipos/script.js
@@ -1,3 +1,5 @@
+//STRINGS
+/* 
 let nome = "João";
 console.log(nome);
 
@@ -54,3 +56,134 @@ console.log(frase.endsWith("?"));
 let fraseModificada = frase.replace(",", "!");
 console.log(frase);
 console.log(fraseModificada);
+ */
+
+//NUMEROS
+/* 
+let num = 100;
+console.log(num+3);
+console.log(num/5);
+console.log(num/3);
+console.log(`Resto ${num%3}`);
+console.log(num);
+//nas alterações acima, não ocorre mudança no valor da variável num, somente o resultado aritmético. Para guardar o resultado, é necessário de uma outra variável recebendo o resultado.
+let numDois = num * 5;
+console.log(numDois);
+
+//Biblioteca Math
+console.log(`Número pi: ${Math.PI}`);
+
+let cincoDivididoPorTres = 5 / 3;
+console.log(`5 / 3 = ${cincoDivididoPorTres}`);
+console.log(`5 / 3 arredondando para baixo: ${Math.floor(cincoDivididoPorTres)}`);
+console.log(`5 / 3 arredondando para cima: ${Math.ceil(cincoDivididoPorTres)}`);
+
+//Para imprimir na tela nº de porcentagem precisa concatenar com string o valor. Exemplo:
+let percent = 10;
+console.log(percent + "%");
+
+//convertendo um nº para uma string
+console.log(percent.toString());//novamente, não altera o original, somente retorna o resultado
+console.log(typeof(percent));
+console.log(typeof(percent.toString()));
+*/
+
+//Booleans
+/* 
+let validacao = 3 == 0;
+console.log(validacao);
+validacao = 3 === 0;
+console.log(validacao);
+validacao = 3 > 2;
+console.log(validacao);
+validacao.toString();
+console.log(typeof(validacao));
+console.log(typeof(validacao.toString()));
+console.log(!validacao);
+*/
+
+//Vetores
+/* 
+//let array = new Array();//outra forma de declarar, não muito utilizada
+let array = [];
+
+//add itens ao array
+console.log("add no final array - push");
+array.push(3);
+console.log(`array.length: ${array.length}`);
+console.log(array);
+array.push("!");
+array.push(5);
+array.push(2);
+array.push('a');
+console.log(`array.length: ${array.length}`);
+console.log(array);
+console.log("add no começo do array - unshift");
+array.unshift('P');//add no começo do array
+console.log(`array.length: ${array.length}`);
+console.log(array);
+
+//removendo itens do array
+console.log("remove o último elemento do array - pop");
+array.pop();//remove o último elemento do array
+console.log(`array.length: ${array.length}`);
+console.log(array);
+console.log("remove o primeiro elemento do array - shift");
+array.shift();//remove o primeiro elemento do array
+console.log(`array.length: ${array.length}`);
+console.log(array);
+
+for(let i = array.length; i >= 0; i--){
+    console.log(array[i])
+}
+*/
+
+//Objetos
+//Estrutura do tipo 'chave/valor' (Map -> em Java)
+/*
+let obj = {};
+console.log(obj);
+obj.name = "julia";
+console.log(obj);
+obj.age = 20;
+console.log(obj);
+
+console.log(Object.values(obj));
+console.log(Object.keys(obj));
+
+let person = {
+    name: "Rulia",
+    age: 20,
+    adress: "rua dois"
+}
+console.log(person);
+//retornando valores
+console.log(person.name);
+console.log(person["name"]);
+//add uma nova entrada chave/valor
+person["qtdDeIrmaos"] = 3;
+console.log(person);
+//add uma nova entrada chave/valor oriunda de uma variável
+let mom = "nomeDaMae";
+person[mom] = "Maria";
+console.log(person);
+*/
+
+//Empty, undefined e null
+
+//undefined
+//abacaxi;
+//console.log(typeof(abacaxi));
+let abacaxi;
+console.log(typeof(abacaxi));
+//empty
+abacaxi = "";
+console.log(typeof(abacaxi));
+//null
+abacaxi = null;
+console.log(typeof(abacaxi));
+
+//maca é undefined, !maca é igual a true, mas maca não é false, na realidade ele é falsy. Pesquisar sobre
+let maca;
+console.log(maca);
+console.log(!maca);
